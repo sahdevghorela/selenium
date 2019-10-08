cd09e20531a7e7dabe6949b03325e990cd3e5b4f
sahdevghorela <sahdevghorela37@gmail.com>
2019-10-08 16:24:22 +0100

    Update LandingPageFacotryTest.java

--- LandingPageFacotryTest.java
+++ LandingPageFacotryTest.java
@@ -40,3 +40,12 @@ public class LandingPageFacotryTest {
         assertTrue(lotteryNumbersPage.firstRowExists());
     }
 }
+Hi,
+
+I hereby, give my consent to forward all medical documentation  for treatment/investigations done on me dated 15/08/2019 at East Surrey Hospital in order to support my insurance claim to sumanchaudhary516@gmail.com. She is my daughter in law and reside at address "12 Gayton court, Somers road, Reigate, RH2 9DX"
+
+PLEASE NOTE:  As my mother is not educated therefore, I (Sahdev Ghorela) have written and read above to her. She has provided her consent for same. 
+
+Thanks and Regards
+Vimla Devi
+(Thumb impression) 
